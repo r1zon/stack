434f482e89e4b2f89ff5dda3def29ead3b4e5c13
r1zon <59363080+r1zon@users.noreply.github.com>
2020-05-10 17:07:19 +0300

    update

--- stack.py
+++ stack.py
@@ -33,58 +33,26 @@ def main():
                          }
         open_brackets = ['(', '{', '[']
         close_brackets = [')', '}', ']']
+        list_brackets = list(list_brackets)
         stack = Stack([])
-        for bracket in list_brackets:
-            stack.push(bracket)
-        if stack.size() % 2 != 0 or stack.peek() in open_brackets:
-            print('Неcбалансированно')
-            continue
-        ex_close_brackets = []
-        ex_open_brackets = []
         k = 0
-        while stack.isEmpty() == False:
-            part_close_brackets = []
-            part_open_brackets = []
-            while stack.peek() in close_brackets:
-                part_close_brackets.append(stack.pop())
-                if stack.isEmpty() == True:
-                    break
-            while stack.peek() in open_brackets:
-                part_open_brackets.append(stack.pop())
-                if stack.isEmpty() == True:
-                    break
-            part_close_brackets.reverse()
-            if len(part_close_brackets) > len(part_open_brackets):
-                delta = len(part_close_brackets) - len(part_open_brackets)
-                for i in reversed(range(len(part_close_brackets)-delta, len(part_close_brackets))):
-                    ex_close_brackets.append(part_close_brackets[i])
-                    part_close_brackets.pop(i)
-            if len(part_close_brackets) < len(part_open_brackets):
-                delta = len(part_open_brackets) - len(part_close_brackets)
-                for i in reversed(range(len(part_open_brackets) - delta, len(part_open_brackets))):
-                    ex_open_brackets.append(part_open_brackets[i])
-                    part_open_brackets.pop(i)
-            for i in range(len(part_close_brackets)):
-                if dict_brackets[part_open_brackets[i]] == part_close_brackets[i]:
-                    continue
-                else:
-                    k += 1
-        if k != 0:
-            print('Неcбалансированно')
-            continue
-        else:
-            # ex_close_brackets.reverse()
-            if len(ex_close_brackets) != len(ex_open_brackets):
+        for i in range(len(list_brackets)):
+            if list_brackets[k] in open_brackets:
+                stack.push(list_brackets[k])
                 k += 1
-            else:
-                for j in range(len(ex_close_brackets)):
-                    if dict_brackets[ex_open_brackets[j]] == ex_close_brackets[j]:
-                        continue
-                    else:
-                        k += 1
-        if k == 0:
+            elif list_brackets[k] in close_brackets:
+                if len(list_brackets) == 1 or k == 0:
+                    stack.push(list_brackets[k])
+                elif dict_brackets[list_brackets[k-1]] == list_brackets[k]:
+                    stack.push(list_brackets[k])
+                    stack.pop()
+                    stack.pop()
+                    list_brackets.pop(k)
+                    list_brackets.pop(k-1)
+                    k -= 1
+        if stack.size() == 0:
             print('Сбалансированно')
         else:
-            print('Неcбалансированно')
+            print('Несбалансированно')
 
 main()
